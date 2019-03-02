0bbf8eebb13669a9848c87ca82d1e2a796e65736
Kresmlin <kycmmas@gmail.com>
2019-03-02 22:05:02 +0000

    spelling error

--- app/controllers/instructor/sections_controller.rb
+++ app/controllers/instructor/sections_controller.rb
@@ -15,7 +15,7 @@ private
 
 def require_authorized_for_current_course
   if current_course.user != current_user
-    render plain: "Unauthorized", status: :unathorized 
+    render plain: "Unauthorized", status: :unauthorized 
   end
 end    
 
