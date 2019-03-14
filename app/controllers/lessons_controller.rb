5d1b7698d10a54833a7c60f79f93681844a0efdd
Kresmlin <kycmmas@gmail.com>
2019-03-14 07:54:23 +0000

    Enrollment for video viewing

--- app/controllers/lessons_controller.rb
+++ app/controllers/lessons_controller.rb
@@ -1,5 +1,10 @@
 class LessonsController < ApplicationController
+  before_action :authenticate_user!
+  before_action :require_authorized_for_current_lesson
+
+
   def show
+
   end
 
   private
@@ -7,4 +12,10 @@ class LessonsController < ApplicationController
   def current_lesson
     @current_lesson ||= Lesson.find(params[:id])
   end  
+
+  def require_authorized_for_current_lesson
+    if current_user.enrolled_in?(current_lesson.section.course) == false 
+    redirect_to course_path(current_lesson.section.course), alert: 'You must enroll in the course to see the lesson'
+   end
+  end 
 end
